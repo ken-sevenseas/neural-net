018d4c55c37e0bfa8ca816ca08b7acabc5afd6df
ken-sevenseas <inakanookaki@icloud.com>
2025-05-25 21:20:44 +0900

    fix: everything

--- lec1/a_extended.c
+++ lec1/a_extended.c
@@ -20,6 +20,7 @@
 #define frand() (rand() % 10000 / 10001.0)
 #define randomize() srand((unsigned int)time(NULL))
 
+//(0,0,-1); (0,1,-1); (1,0,-1); (1,1,-1)
 double x[n_sample][I] = {
     {0, 0, -1},
     {0, 1, -1},
@@ -28,11 +29,7 @@ double x[n_sample][I] = {
 };
 
 double w[I];
-double d[n_sample] = {0, 0, 0, 1};
-/*You can change the output values
-    or 0, 0, 0, 1 for OR gate
-    or 1, 1, 1, -1 for NAND gate
-*/
+double d[n_sample] = {-1, -1, -1, 1};
 double o;
 
 void deltaLearning(void);
@@ -41,7 +38,33 @@ void Initialization(void);
 void FindOutput(int);
 void FindOutputPerceptron(int);
 void PrintResult(void);
-void printNeuronOutput(int);
+void printNeuronOutput();
+
+// int main() {
+//     int i, p, q = 0;
+//     double LearningSignal = 1.0, Error = DBL_MAX;
+
+//     Initialization();
+//     printf("The initial connection weights of the neurons:\n");
+//     printNeuronOutput();
+//     printf("\n\n");
+
+//     while (Error > desired_error) {
+//         q++;
+//         Error = 0;
+//         for (p = 0; p < n_sample; p++) {
+//             FindOutputPerceptron(p);
+//             Error += 0.5 * pow(d[p] - o, 2.0);
+//             LearningSignal = eta * (d[p] - o);
+//             for (i = 0; i < I; i++) {
+//                 w[i] += LearningSignal * x[p][i];
+//             }
+//         }
+//         printf("Error in the %d-th learning cycle=%f\n", q, Error);
+//     }
+//     PrintResult();
+//     printNeuronOutput();
+// }
 
 int main() {
     deltaLearning();
@@ -58,8 +81,8 @@ void deltaLearning() {
 
     Initialization();
     printf("The initial connection weights of the neurons:\n");
-    PrintResult();
-    printNeuronOutput(0);
+    printNeuronOutput();
+    printf("\n\n");
 
     while (Error > desired_error) {
         q++;
@@ -71,11 +94,11 @@ void deltaLearning() {
                 delta = (d[p] - o) * (1 - o * o) / 2;
                 w[i] += eta * delta * x[p][i];
             }
+            printf("Error in the %d-th learning cycle=%f\n", q, Error);
         }
-        printf("Error in the %d-th learning cycle=%f\n", q, Error);
     }
     PrintResult();
-    printNeuronOutput(0);
+    printNeuronOutput();
 }
 
 void perceptronLearning() {
@@ -83,11 +106,9 @@ void perceptronLearning() {
     double LearningSignal = 1.0, Error = DBL_MAX;
 
     Initialization();
-    printf(
-        "######################## The initial connection weights of the "
-        "neurons ######################## ");
-    PrintResult();
-    printNeuronOutput(1);
+    printf("The initial connection weights of the neurons:\n");
+    printNeuronOutput();
+    printf("\n\n");
 
     while (Error > desired_error) {
         q++;
@@ -103,7 +124,7 @@ void perceptronLearning() {
         printf("Error in the %d-th learning cycle=%f\n", q, Error);
     }
     PrintResult();
-    printNeuronOutput(1);
+    printNeuronOutput();
 }
 
 /*************************************************************/
@@ -142,39 +163,24 @@ void PrintResult(void) {
     int i;
 
     printf("\n\n");
-    printf("The connection weights of the neurons \n");
+    printf("The connection weights of the neurons:\n");
     for (i = 0; i < I; i++) printf("%5f ", w[i]);
-    // printf("\n");
+    printf("\n\n");
 }
 
-void printNeuronOutput(int type) {
-    // type = 1: perceptron learning, type = 2: delta learning;
+void printNeuronOutput() {
     int p, i;
     double u;
+    int output;
 
     printf("\nNeuron output for each input pattern:\n");
-    if (type == 0) {
-        double output;
-        for (p = 0; p < n_sample; p++) {
-            u = 0.0;
-            for (i = 0; i < I; i++) {
-                u += w[i] * x[p][i];
-            }
-            output = sigmoid(u);
-            printf("Input (%f, %f, %f) -> Output: %f (Teacher: %f)\n", x[p][0],
-                   x[p][1], x[p][2], output, d[p]);
+    for (p = 0; p < n_sample; p++) {
+        u = 0.0;
+        for (i = 0; i < I; i++) {
+            u += w[i] * x[p][i];
         }
-    } else {
-        int output;
-        for (p = 0; p < n_sample; p++) {
-            u = 0.0;
-            for (i = 0; i < I; i++) {
-                u += w[i] * x[p][i];
-            }
-            output = (u > 0) ? 1 : -1;
-            printf("Input (%f, %f, %f) -> Output: %d (Teacher: %f)\n", x[p][0],
-                   x[p][1], x[p][2], output, d[p]);
-        }
+        output = (u > 0) ? 1 : -1;
+        printf("Input (%f, %f, %f) -> Output: %d (Teacher: %f)\n", x[p][0],
+               x[p][1], x[p][2], output, d[p]);
     }
-    printf("\n");
 }
